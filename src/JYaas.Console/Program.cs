d31b4f94d4f34ae9efc9ae7ff893146681ccb95b
Michael Terry <mterry@sancsoft.com>
2017-07-12 00:30:05 -0400

    console test app

---
+++ src/JYaas.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using CallfireApiClient;
+using CallfireApiClient.Api.CallsTexts.Model;
+
+namespace JYaas
+{
+    class Program
+    {
+        static bool IsPhoneNumber(string number)
+        {
+            return Regex.Match(number, @"^1([0-9]{10})$").Success;
+        }
+
+        static void Main(string[] args)
+        {
+            var client = new CallfireClient(ConfigurationManager.AppSettings["CallFireUser"], ConfigurationManager.AppSettings["CallFirePassword"]);
+
+            if ((args.Length == 0) || (!IsPhoneNumber(args[0])))
+            {
+                Console.WriteLine("Usage: jyaas.console.exe [phonenumber:1234567890]");
+                return;
+            }
+
+            var recipients = new List<CallRecipient>
+            {
+                new CallRecipient
+                {
+                    PhoneNumber = args[0],
+                    LiveMessageSoundId = 5743849003,
+                    MachineMessageSoundId = 5743849003
+                }
+            };
+            IList<Call> calls = client.CallsApi.Send(recipients);
+        }
+    }
+}
